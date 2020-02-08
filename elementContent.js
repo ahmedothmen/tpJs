0d7983c1ae8db1f1b0005c0630569ed2f601c5e7
ahmedothmen <ahmed.benothmen@esprit.tn>
2020-02-08 22:18:49 +0100

    second tuto formatio

---
+++ elementContent.js
@@ -0,0 +1,10 @@
+/*
+innerText mch standard mete5demech bihom
+ourText
+innerHtml  3adi e5dem bihom dji tous le contenu html
+ourterHtml
+textContent hedhia mansou7 biha tjib text content
+*/
+
+var mydiv=document.getElementById('main');
+mydiv.innerHTML=`hedhe text jdida <span style="color:red">Bouhmid</span>`
--- test.js
+++ test.js
@@ -1,3 +1,14 @@
+/*
+document.title
+document.images
+document.forms
+document.body
+indexOf('') ta3delha chaine de caractere traja3 l index mte7a
+*/
+
 var mypdev=document.getElementById('test');
 var content=document.getElementById('content');
-mypdev.innerHTML=document.forms[1].x3.value;
+if (document.body.innerHTML.includes('ggg')){
+    console.log('mawjouda bouhmid');
+}
+ mypdev.innerHTML=document.links.length
