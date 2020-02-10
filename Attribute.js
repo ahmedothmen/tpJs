7770879609eadba9cb7e37df4a6b9f25c6bfe297
ahmedothmen <ahmed.benothmen@esprit.tn>
2020-02-10 22:43:34 +0100

    decouvrir setAttribute Methode javascript

--- Attribute.js
+++ Attribute.js
@@ -5,6 +5,4 @@ mydiv.getAttribute('className'); dji l'atribut
 */
 var mydiv=document.querySelector('div');
 
-
-
 mydiv.setAttribute('class','test1');  
---
+++ RemoveAttribute.js
@@ -0,0 +1,4 @@
+var mydv=document.createElement('div');
+mydv.setAttribute('id','test');
+mydv.innerHTML="<p style='color:red'> ahmed </p>"
+document.body.append(mydv);
