1aaef984e037519cbf4fa5f9cdf32125dceee3c7
ahmedothmen <ahmed.benothmen@esprit.tn>
2020-02-10 23:04:28 +0100

    decouvrir classList classList.contais classList.index

--- RemoveAttribute.js
+++ RemoveAttribute.js
@@ -2,3 +2,8 @@ var mydv=document.createElement('div');
 mydv.setAttribute('id','test');
 mydv.innerHTML="<p style='color:red'> ahmed </p>"
 document.body.append(mydv);
+var mainDiv=document.getElementById('main');
+if(mainDiv.classList.contains('cool')){
+    console.log('yes it s found');
+}
+console.log(mainDiv.classList.item(1));
